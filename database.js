4960d401b265a3d3d55e474f20e4fa4c3672f835
unknown <abhinavsingh021094@gmail.com>
2023-10-30 20:16:12 -0700

    first commit

---
+++ database.js
@@ -0,0 +1,35 @@
+// db.js
+const mongoose = require('mongoose');
+
+const MONGO_URI = 'mongodb+srv://AbhinavSingh:NapX@cluster0.kgw1q72.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+  });
+  
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+  
+db.once('disconnected', () => {
+    console.log('Disconnected from MongoDB');
+});
+  
+db.on('reconnected', () => {
+    console.log('Reconnected to MongoDB');
+});
+  
+db.on('close', () => {
+    console.log('Connection to MongoDB closed');
+});
+  
+
+module.exports = db;
